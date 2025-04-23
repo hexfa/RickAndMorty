8e12632fb8291c1ae064781c540f7fe128f6900f
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-23 17:49:36 +0300

    [REFACTOR] Add test for full field conversion in toCharacter()

--- app/src/test/java/com/hexfa/rickandmorty/data/mapper/CharactersMapperTest.kt
+++ app/src/test/java/com/hexfa/rickandmorty/data/mapper/CharactersMapperTest.kt
@@ -5,5 +5,26 @@ import org.junit.Assert
 import org.junit.Test
 
 class CharacterMapperTest {
-    
+
+    @Test
+    fun `test toCharacter should return valid Character when all fields are present`() {
+        // Arrange
+        val result = CharactersQuery.Result(
+            name = "Rick Sanchez",
+            image = "rick_image_url",
+            id = "1",
+            gender = "Male",
+            species = "Human"
+        )
+
+        // Act
+        val character = result.toCharacter()
+
+        // Assert
+        Assert.assertEquals("Rick Sanchez", character.name)
+        Assert.assertEquals("rick_image_url", character.image)
+        Assert.assertEquals("1", character.id)
+        Assert.assertEquals("Male", character.gender)
+        Assert.assertEquals("Human", character.species)
+    }
 }
