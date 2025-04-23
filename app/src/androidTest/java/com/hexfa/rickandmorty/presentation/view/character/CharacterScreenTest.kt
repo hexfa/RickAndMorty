def52451fae58db0a55b49cc6ce81eda41b38e98
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-23 18:17:51 +0300

    [REFACTOR] Implement UI test for CharacterScreenTest

---
+++ app/src/androidTest/java/com/hexfa/rickandmorty/presentation/view/character/CharacterScreenTest.kt
@@ -0,0 +1,39 @@
+package com.hexfa.rickandmorty.presentation.view.character
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.hexfa.rickandmorty.MainActivity
+import com.hexfa.rickandmorty.presentation.view.setting.SettingViewModel
+import com.hexfa.rickandmorty.ui.theme.RickAndMortyTheme
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class CharacterScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private lateinit var fakeCharactersViewModel: CharactersViewModel
+    private lateinit var fakeSettingViewModel: SettingViewModel
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+
+        composeTestRule.setContent {
+            RickAndMortyTheme {
+                CharactersScreen(settingViewModel = fakeSettingViewModel)
+            }
+        }
+    }
+}
